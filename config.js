3aac4baad31a06198206f347bffeeeba7c56b4e6
ᴍ ʀ ⃞ ɪ ꜱ ᴜ ʀ ᴜ <156693573+MrIsuru@users.noreply.github.com>
2024-02-23 13:46:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,26 +10,26 @@ global.antitags = true;
 
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = true;                //auto recording by default off.
+global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
 //
-global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+global.prefa = ["$"];                                    //Default prefix here. you can change if you want.
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["94766632281", "+94766632281"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["94766632281", "+94766632281"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["94766632281"];
+global.OwnerName = "Mʀ Iꜱᴜʀᴜ X";
+global.BotName = "Cʏʙᴇʀ Pᴀɴᴅᴀ-MD";
+global.packname = "ᴍͥ ʀ  ɪ ꜱͣ ᴜͫ ʀ ᴜ  x";                             //Do not change.
+global.author = "ᴇᴢʀ - ᴍͥ ʀ  ɪ ꜱͣ ᴜͫ ʀ ᴜ  x";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/Ko2HfqBt5mjEsmV9fY7Bsq";
 
 
 //
@@ -41,9 +41,9 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "mathara, Sri Lankan";
-global.reactmoji = "💞";
-global.themeemoji = "😂";
+global.location = "ɢᴀʟʟᴇ, Sri Lankan";
+global.reactmoji = "💀";
+global.themeemoji = "💀";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
 global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
 global.lolhuman = "kushan";
